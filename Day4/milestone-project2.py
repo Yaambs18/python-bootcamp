51b1b403aa40a52ced71a881c16012caaf62a90e
yaambs <yanshbhardwaj14@gmail.com>
2021-09-13 16:26:43 +0530

    project2 complete code updated

--- Day4/milestone-project2.py
+++ Day4/milestone-project2.py
@@ -33,7 +33,7 @@ class Deck:
     def shuffle(self):
         random.shuffle(self.deck)
 
-    def deal_one(self):
+    def deal(self):
         return self.deck.pop()
 
 class Hand:
@@ -139,3 +139,62 @@ def dealer_wins(player,dealer,chips):
     
 def push(player,dealer):
     print("Dealer and Player tie! It's a push.")
+
+while True:
+    print('Welcome to BlackJack! \n Get as close to 21 as you can without going over!\n\
+    Dealer hits until she reaches 17. Aces count as 1 or 11.')
+    
+    deck = Deck()
+    deck.shuffle()
+    
+    player_hand = Hand()
+    player_hand.add_cards(deck.deal())
+    player_hand.add_cards(deck.deal())
+    
+    dealer_hand = Hand()
+    dealer_hand.add_cards(deck.deal())
+    dealer_hand.add_cards(deck.deal())
+            
+    
+    player_chips = Chip()   
+    take_bet(player_chips)
+    
+    show_some(player_hand,dealer_hand)
+    
+    while playing: 
+        hit_or_stand(deck,player_hand) 
+        show_some(player_hand,dealer_hand)  
+        if player_hand.value > 21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break        
+
+ 
+    if player_hand.value <= 21:
+        
+        while dealer_hand.value < 17:
+            hit(deck,dealer_hand)    
+    
+        show_all(player_hand,dealer_hand)
+        
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+
+        else:
+            push(player_hand,dealer_hand)        
+    
+    print("\nPlayer's winnings stand at",player_chips.total)
+    
+    new_game = input("Would you like to play another hand? Enter 'y' or 'n' ")
+    
+    if new_game[0].lower()=='y':
+        playing=True
+        continue
+    else:
+        print("Thanks for playing!")
+        break
