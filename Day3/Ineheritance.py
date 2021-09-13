db3a9d0e7574e3130273af645a8d98df5238a40e
yaambs <yanshbhardwaj14@gmail.com>
2021-09-13 11:17:47 +0530

    hands-on inheritance

---
+++ Day3/Ineheritance.py
@@ -0,0 +1,64 @@
+#Multilevel Inheritance
+
+class Base():
+	
+	def __init__(self, name):
+		self.name = name
+
+	def getName(self):
+		return self.name
+
+class Child(Base):
+	
+	def __init__(self, name, age):
+		Base.__init__(self, name)
+		self.age = age
+
+	def getAge(self):
+		return self.age
+
+class GrandChild(Child):
+	
+	def __init__(self, name, age, address):
+		Child.__init__(self, name, age)
+		self.address = address
+
+	def getAddress(self):
+		return self.address		
+
+m = GrandChild("Geek1", 23, "Noida")
+print(m.getName(), m.getAge(), m.getAddress())
+
+
+
+#Mutiple inheritance
+
+class Addition():
+    def __init__(self):
+        print("addition")  
+    def Addition(self,a,b):  
+        return a+b;  
+    def func(self):
+        print("this method is of addition class")
+
+class Mulultiplication():  
+    def __init__(self):
+        print("multiplication")  
+    def Multiplication(self,a,b):  
+        return a*b
+    def m1(self):
+        print("this method is of multipication  class")
+      
+class Division(Addition, Mulultiplication):
+    def __init__(self):
+        print("divide")  
+    def Divide(self,a,b):  
+        return a/b;  
+
+
+
+divide_ = Division()  
+print(divide_.Addition(10,20))  
+print(divide_.Multiplication(10,20))  
+print(divide_.Divide(10,20))  
+divide_.m1()
