40a158e35dd84cefc2675f79de116e4a37586f3c
yaambs <yanshbhardwaj14@gmail.com>
2021-09-13 17:33:32 +0530

    unit-test third code update

--- Day4/unit-test5.py
+++ Day4/unit-test5.py
@@ -5,12 +5,12 @@ import cylinder_class
 
 class Test_volume_sa(unittest.TestCase):
     def test_volume(self):
-        c = cylinder_class.Cylinder(2,3)
+        c = cylinder_class.Cylinder(2, 3)
         result = c.volume()
         self.assertEqual(result, 56.52)
 
     def test_sa(self):
-        c = cylinder_class.Cylinder(2,3)
+        c = cylinder_class.Cylinder(2, 3)
         result = c.surface_area()
         self.assertEqual(result, 94.2)    
 
--- Day4/unit_test3.py
+++ Day4/unit_test3.py
@@ -8,8 +8,8 @@ class TestCap(unittest.TestCase):
         ob2 = laptop_class.Laptop('hp', 'axus', 55000)
         result1 = ob1.apply_discount(40)
         result2 = ob2.apply_discount(50)
-        self.assertEqual(int(result1), 27000 )
-        self.assertEqual(int(result2), 27500 )
+        self.assertEqual(int(result1), 27000)
+        self.assertEqual(int(result2), 27500)
 
 if __name__ == '__main__':
     unittest.main()
