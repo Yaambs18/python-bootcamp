0022dbbaa86058fcc6b0f0cfcce7be7dace181c2
yaambs <yanshbhardwaj14@gmail.com>
2021-09-16 11:29:09 +0530

    email, pdf, csv, advance objects file added

---
+++ Day6/pdf_reader.py
@@ -0,0 +1,15 @@
+import PyPDF2
+
+f = open('Python Crash Course ( PDFDrive.com ).pdf', 'rb')
+
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+print(pdf_reader.numPages)
+
+page_one = pdf_reader.getPage(0)
+
+page_data = page_one.extractText()
+
+print(page_data)
+
+f.close()
