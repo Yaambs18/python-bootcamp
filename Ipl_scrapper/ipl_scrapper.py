f7fbfcb1a4eb6afa6d3aa69928b3468be3ebd8e3
yaambs <yanshbhardwaj14@gmail.com>
2021-09-21 16:38:22 +0530

    ipl team data web scraper created

---
+++ Ipl_scrapper/ipl_scrapper.py
@@ -0,0 +1,39 @@
+import requests,bs4
+import csv
+
+base_url = 'https://www.iplt20.com/teams/men'
+res = requests.get(base_url)
+soup = bs4.BeautifulSoup(res.text,'lxml')
+
+product_card = soup.select('.card__details')
+product_title = soup.select('.card__title')
+product_subtitle = soup.select('.card__subtitle')
+product_year = soup.select('.team-card__wins')
+win_year = []
+j = 0
+i = 0
+while j < 8:
+    if product_card[j].find('div'):
+        if i<5:
+            win_year.append(product_year[i].get_text().strip())
+            i+=1
+        j+=1
+    else :
+        win_year.append('Not win yet')
+        j+=1
+
+team_data_dictionary = {}
+for i in range(1,9):
+    team_data_dictionary[f'{i}'] = {}
+for i in range(8):
+    team_data_dictionary[f'{i+1}']['title'] = product_title[i].getText()
+    team_data_dictionary[f'{i+1}']['venue'] = product_subtitle[i].getText().strip()
+    team_data_dictionary[f'{i+1}']['winning_year'] = win_year[i]
+
+
+fields = ['S.no', 'title', 'venue', 'winning_year']
+with open("movies_data.csv", "w") as csvfile:
+            writer = csv.DictWriter(csvfile, fields)
+            writer.writeheader()
+            for key in team_data_dictionary:
+                writer.writerow({field: team_data_dictionary[key].get(field) or key for field in fields})
